bf01ee01f81771f4b45e95baeddb7e0b57858100
fowwo <54595444+fowwo@users.noreply.github.com>
2023-03-20 03:01:55 -0500

    Add puzzle creation functionality

--- midterm/editor/board.js
+++ midterm/editor/board.js
@@ -1,5 +1,45 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+let board = null;
+
+// Manual puzzle creation form
+document.getElementById("puzzle-input").onsubmit = (event) => {
+	event.preventDefault();
+
+	const errorMessage = event.target.querySelector("p");
+
+	try {
+		board = parseBoard(event.target.querySelector("textarea").value);
+	} catch (error) {
+		errorMessage.innerText = error;
+		errorMessage.style.display = "";
+		return;
+	}
+	
+	errorMessage.style.display = "none";
+	displayBoard(board);
+};
+
+function parseBoard(string) {
+	const board = string.toLowerCase()
+		.replace(/[\r\n]+/g, "\n") // Remove carriage returns
+		.replace(/[^\S\n]+/g, "")  // Remove non-newline whitespace
+		.split(/[\n\/]/)           // Split on newline or slash
+		.map(x => x.split(""))     // Split strings into letter arrays
+		.filter(x => x.length);    // Remove empty rows
+
+	// Validate
+	const width = board[0].length;
+	for (const row of board) {
+		if (row.length !== width) throw Error("Each row must have the same number of characters.");
+		for (const letter of row) {
+			if (!(alphabet + ".").includes(letter)) throw Error(`'${letter}' is an invalid character.`);
+		}
+	}
+
+	return board;
+}
+
 function displayBoard(board) {
 	const boardElement = document.getElementById("board");
 	boardElement.innerHTML = "";
